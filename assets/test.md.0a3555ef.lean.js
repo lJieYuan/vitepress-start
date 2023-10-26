09c9dfeb35bad6d9cdf1ceb3e163515c52220fb0
yuanjie <yuanjie@zkstarroad.com>
2023-10-26 17:14:09 +0800

    github actions 自动化部署1

---
+++ assets/test.md.0a3555ef.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as n,Q as e}from"./chunks/framework.8c2f18ed.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"test.md","filePath":"test.md"}'),t={name:"test.md"},o=e("",2),p=[o];function l(c,r,i,E,d,_){return a(),n("div",null,p)}const u=s(t,[["render",l]]);export{h as __pageData,u as default};
